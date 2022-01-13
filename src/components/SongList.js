0c7da96f98b8d2a7e26deac711ccf320c6764ad1
sruthi-lal <lalsruthi145@gmail.com>
2022-01-13 12:16:16 +0530

    song added

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,28 @@
  import React, { Component } from 'react'
  import { connect } from 'react-redux'
  export class SongList extends Component {
+          renderList(){
+              return this.props.song.map((s) =>{
+                  return(
+                      <div key={s.title}>
+                          <div>
+                              <button>Select</button>
+                          </div>
+                          <div>
+                              {s.title}
+                          </div>
+
+                      </div>
+                  )
+
+              })
+          }
+
      render() {
            console.log(this.props)
          return (
              <div>
-                 SongList
+                 {this.renderList()}
              </div>
          )
      }
